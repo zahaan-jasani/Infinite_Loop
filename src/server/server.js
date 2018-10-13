0a6b891ccd57812c9baba0131580cc97e302ed6d
Oscar Chan <chanoscar0@gmail.com>
2018-10-12 19:52:12 -0700

    Testing for Server

--- src/server/server.js
+++ src/server/server.js
@@ -32,6 +32,8 @@ app.patch('/status',
 app.use((err, req, res, next) => {
   res.status(400).send(err);
 });
+app.use(express.static(__dirname + '/../../dist'));
+
 app.listen(3000, () => {
   console.log('Listening on port 3000...');
 } );
