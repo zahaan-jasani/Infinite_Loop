bc0a0492627801b00ec9a471726505ed39c11bf2
Pablo Lee <pablolee@Pablos-MacBook-Pro.local>
2018-10-12 20:20:56 -0700

    add patch testing

--- __test__/userController.test.js
+++ __test__/userController.test.js
@@ -16,14 +16,14 @@ describe('Test the root path', () => {
 describe('post', () => {
   it('responds to valid request with 200 status and application/json content type', (done) => {
     request(HOST)
-      .get('/games')
+      .get('/home')
       .expect('Content-Type', /application\/json/)
       .expect(200, done);
   });
   it('should return the object created in the database', (done) => {
-    const testObj = { name: 'Pablo' };
+    const testObj = { name: 'Pablo', role: "user" };
     request(HOST)
-      .post('/createUser')
+      .post('/createuser')
       .send(testObj)
       .then((res) => {
         expect(res.body.name).toEqual(testObj.name);
@@ -33,10 +33,25 @@ describe('post', () => {
   it('responds to invalid request with error and 400 status', (done) => {
     const testObj = {name: "hello", test: "hi"};
     request(HOST)
-      .post('/createUser')
+      .post('/createuser')
       .send(testObj)
       .then((res) => {
-        expect(res.status).toBe('400');
+        expect(res.status).toBe(400);
+        done()
       })
   })
 });
+
+describe('patch', ()=> {
+  it('responds to valid request with 200 status and application/json type', (done) => {
+    const statusObj = {id: 1, status: 2};
+    request(HOST)
+      .patch('/status')
+      .send(statusObj)
+      .then((res) => {
+        expect(res.status).toBe(400);
+        done()
+      })
+  })
+  it('it should return the updated object to the client')
+})
--- src/server/controller/userController.js
+++ src/server/controller/userController.js
@@ -1,26 +1,40 @@
-const db = require('../postgresql.js')
+const db = require('../postgresql.js');
+
 module.exports = {
+ /**
+   * creates new user record and returns the created record
+   * @param { Object } req request object 
+   * @param { object } res response object
+   * @param { function } next function to go to next middlewear 
+   */
   createUser(req, res, next) {
-    if(req.body.role === 'user') {
-      db.one("INSERT INTO student(name) VALUES ($1) RETURNING (name)", req.body.name)
-      .then(data => {
-        res.locals.data = data;
-        return next();
-      })
-      .catch(err => {
-        console.log('ERROR: ', err)
-        return next(err);
-      });
-    } else{
-      db.one("INSERT INTO helper(name) VALUES ($1) RETURNING (name)", req.body.name)
-      .then(data => {
-        res.locals.data = data;
-        return next();
-      })
-      .catch(err => {
-        console.log('ERROR: ', err)
-        return next(err);
-      });
+    //check request has correct body format
+    if(Object.keys(req.body).length === 2 && req.body.name && req.body.role) {
+      //if role equals to user, create user 
+      if(req.body.role === 'user') {
+        db.one("INSERT INTO student(name) VALUES ($1) RETURNING (name)", req.body.name)
+        .then(data => {
+          res.locals.data = data;
+          return next();
+        })
+        .catch(err => {
+          console.log('ERROR: ', err)
+          return next(err);
+        });
+      } else{
+        // if not user create helper
+        db.one("INSERT INTO helper(name) VALUES ($1) RETURNING (name)", req.body.name)
+        .then(data => {
+          res.locals.data = data;
+          return next();
+        })
+        .catch(err => {
+          console.log('ERROR: ', err)
+          return next(err);
+        });
+      }
+    } else {
+      return res.status(400).send();
     }
   },
   verifyUser(req, res, next) {
