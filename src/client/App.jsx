6bacf4a5ac9262cb6adbaa5d36e935307d49c5ce
Oscar Chan <chanoscar0@gmail.com>
2018-10-12 20:17:26 -0700

    React-skeleton

---
+++ src/client/App.jsx
@@ -0,0 +1,21 @@
+import react, { Component } from 'react';
+
+import PostSection from './components/PostSection.jsx';
+import Signup from './components/Signup.jsx';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      name: '',
+      notStarted: [],
+      inProgress: [],
+      closed: [],
+      role: '',
+      loggedIn: false
+    };
+  }
+  render() {
+  }
+}
+export default App;
