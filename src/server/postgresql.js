dac42a4f9707c775fc60eb3b5cd97bc70b9cea24
g-es <gesun@usc.edu>
2018-10-15 15:30:42 -0700

    change pg url

--- src/server/postgresql.js
+++ src/server/postgresql.js
@@ -1,4 +1,4 @@
-const connection = 'postgres://lrbqolbi:AoiXpqFJzYSWj75UH2IgIppEF8QLPzGO@elmer.db.elephantsql.com:5432/lrbqolbi';
+const connection = 'postgres://rxhbrrie:nAsV0LK2CtUTm7S5L2IGj52SEcgiSGgE@elmer.db.elephantsql.com:5432/rxhbrrie';
 const pgp = require('pg-promise')(/*options*/);
 const db = pgp(connection);
 
